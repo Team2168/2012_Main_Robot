a6af87de89cecde902e4dce638c2722325ff0395
srinnin19 <srinnin@yahoo.com>
2012-01-25 18:14:05 -0500

    Initial Drive train subsystem - not working

---
+++ 2012_Main_Robot/src/frc2168/subsystems/DriveTrain.java
@@ -0,0 +1,12 @@
+package frc2168.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveTrain extends Subsystem {
+
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
