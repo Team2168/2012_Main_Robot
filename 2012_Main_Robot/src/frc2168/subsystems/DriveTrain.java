4bdf3eb63422189b78015e94acd8bda465773bac
srinnin19 <srinnin@yahoo.com>
2012-01-25 18:16:23 -0500

    Added comments - not working

--- 2012_Main_Robot/src/frc2168/subsystems/DriveTrain.java
+++ 2012_Main_Robot/src/frc2168/subsystems/DriveTrain.java
@@ -2,6 +2,11 @@ package frc2168.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
+/**
+ * This is a sample class subsystem
+ * @author Owner
+ *
+ */
 public class DriveTrain extends Subsystem {
 
 	protected void initDefaultCommand() {
