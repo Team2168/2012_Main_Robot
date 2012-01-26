98b3ae81acb8d9974fda729ebecd0ff43f14f0c6
srinnin19 <srinnin@yahoo.com>
2012-01-25 19:04:09 -0500

    default command - not working

--- 2012_Main_Robot/src/frc2168/subsystems/DriveTrain.java
+++ 2012_Main_Robot/src/frc2168/subsystems/DriveTrain.java
@@ -1,6 +1,9 @@
 package frc2168.subsystems;
 
+import edu.wpi.first.wpilibj.can.CANTimeoutException;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.*;
+
 
 /**
  * This is a sample class subsystem
@@ -8,10 +11,32 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class DriveTrain extends Subsystem {
+	CANJaguar CANJag;
+	Joystick driverStick;
+	
 
 	protected void initDefaultCommand() {
+		driverStick = new Joystick (1);
+		
+		try {
+			CANJag = new CANJaguar (1);
+			} 
+		catch (CANTimeoutException ex) {
+            ex.printStackTrace();
 		// TODO Auto-generated method stub
+      
+        
 
+		}
+
+
+		 driverStick.getRawAxis(2);
+		 
+		try {CANJag.setX(1);
+			 } 
+		catch (CANTimeoutException ex) {
+				 ex.printStackTrace ();
+			 }
+		}
 	}
 
-}
