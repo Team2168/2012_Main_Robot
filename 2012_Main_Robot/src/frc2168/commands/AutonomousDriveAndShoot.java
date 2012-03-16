78ad02779d5033cc2f2b025af8932ba13f316957
user <user@184.49.152.79>
2012-03-16 02:27:32 -0400

    consolidated autonomous modes so use target selector, fixed minor bugs, name scheme with autonomous and auto

---
+++ 2012_Main_Robot/src/frc2168/commands/AutonomousDriveAndShoot.java
@@ -0,0 +1,43 @@
+package frc2168.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import frc2168.RobotMap;
+
+public class AutonomousDriveAndShoot extends CommandGroup
+{
+	public AutonomousDriveAndShoot(boolean target){
+		
+		//go up to bumper
+		addSequential(new DriveToSpeed(0.6),1);		//change value according to testing.
+		
+		//raise bridge arm
+		addSequential(new RaiseBridge());
+		
+		//raise flap to allow the elevator to pull balls up
+		addSequential(new BackFlapClose());
+		
+		//Shift the drive train to high gear
+		addSequential(new ShiftGearsLowToHigh());
+		
+		if(target == RobotMap.HIGH_GOAL){
+		//shoot one ball to high goal
+		addSequential(new HighGoalAuto());
+		
+		//shoot one ball to high goal
+		addSequential(new HighGoalAuto());
+		}
+		
+		if(target == RobotMap.MIDDLE_GOAL){
+			//shoot one ball to high goal
+			addSequential(new MiddleGoalAuto());
+			
+			//shoot one ball to high goal
+			addSequential(new MiddleGoalAuto());
+		}
+		
+		addSequential(new sleep(),1);
+		
+	}
+	
+	
+}
